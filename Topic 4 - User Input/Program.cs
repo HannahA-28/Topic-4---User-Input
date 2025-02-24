2de6440cebd1116a1d3f9f096404c4819d202e91
Hannah Anderson <339729345@GoTVDSB.ca>
2025-02-24 14:40:35 -0500

    Onto programming task

--- Topic 4 - User Input/Program.cs
+++ Topic 4 - User Input/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int age;
+            //Console.WriteLine("Enter your age:");
+            //age = Console.ReadLine();
+            //Console.WriteLine("Your age is: " + age);
+            //Console.ReadLine();
+
+            //int age;
+            //Console.WriteLine("Enter your age:");
+            //age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your age is: " + age);
+            //Console.ReadLine();
+
+            //int age;
+            //Console.WriteLine("Enter your age:");
+            //Console.WriteLine(Int32.TryParse(Console.ReadLine(), out age));
+            //Console.WriteLine("Your age is: " + age);
+            //Console.ReadLine();
+
+            //double price, discount;
+            //Console.WriteLine("What is the price?");
+            //Double.TryParse(Console.ReadLine(), out price);
+            //discount = price * 0.2;
+            //Console.WriteLine("The discounted price is " + (price - discount).ToString("C"));
+
+            int pizza;
+            Console.WriteLine("What is your name and favourite pizza topping?");
+
+
+            double price;
         }
     }
 }
